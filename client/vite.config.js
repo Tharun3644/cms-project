5bf0a487903dc2c68efc4868dc081cad2820ce9b
madhan8651 <22becse050@kahedu.edu.in>
2025-05-28 10:15:32 +0000

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    allowedHosts:["5174-tharun3644-cms-project-9icubl8tlx.app.codeanywhere.com"],
-    
+    allowedHosts:["5175-tharun3644-cms-project-9icubl8tlx.app.codeanywhere.com"],
+  
   },
-  plugins: [react(),tailwindcss]
+  plugins: [react(),tailwindcss()]
 })
