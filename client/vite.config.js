3c9aefa75adc791da8dd178480f922a74675073e
madhan8651 <22becse050@kahedu.edu.in>
2025-05-28 06:39:18 +0000

    home selected

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server:{
+    allowedHosts:["5174-tharun3644-cms-project-9icubl8tlx.app.codeanywhere.com"],
+    
+  },
+  plugins: [react(),tailwindcss]
 })
